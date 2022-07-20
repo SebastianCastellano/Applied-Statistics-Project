195175311454752010e0dc148ff6e7248bea7694
giuliabergonzoli <108860743+giuliabergonzoli@users.noreply.github.com>
2022-07-20 12:58:18 +0200

    clusters

--- Scripts/Clustering/clustering_bis.R
+++ Scripts/Clustering/clustering_bis.R
@@ -19,7 +19,7 @@ countries <- list(ITA, AUT, BEL, DNK, DEU, LUX, ESP, SWE, CHE, GBR, EUR)
 name_countries <- c("ITA", "AUT", "BEL", "DNK", "DEU", "LUX", "ESP", "SWE", "CHE", "GBR",'EUR')
 n_countries <- length(countries)
 
-varID = c('math', 'ESCS_status', 'learn_time_math')
+varID = c('math', 'ESCS_status', 'learn_time_math', 'short_edu_mat', 'read', 'learn_time_read', 'short_edu_staff', 'stu_behav', 'teach_behav')
 
 # MATH AND WEALTH VARIABLES----
 
@@ -28,7 +28,7 @@ varID = c('math', 'ESCS_status', 'learn_time_math')
 b <- NULL
 w <- NULL
 for(k in 1:10){
-  result.k <- kmeans(dataset_dnk, k)
+  result.k <- kmeans(DNK, k)
   w <- c(w, sum(result.k$wit))
   b <- c(b, result.k$bet)
 }
@@ -63,9 +63,60 @@ for (i in 1:n_countries) {
   # Graphical Representation
   par(mfrow = c(2,2))
   boxplot(math~cluster, Y, col = "gold", main = name_countries[i])
-  boxplot(learn_time_math~cluster, Y, col = "gold", main = name_countries[i])
+  boxplot(read~cluster, Y, col = "gold", main = name_countries[i])
   boxplot(ESCS_status~cluster, Y, col = "gold", main = name_countries[i])
   barplot(immigFrac, names.arg = c('1','2','3')
           , ylim = c(0,1), main = 'Fraction of immigrants in each cluster')
+  boxplot(short_edu_staff~cluster, Y, col = "gold", main = name_countries[i])
+  boxplot(short_edu_mat~cluster, Y, col = "gold", main = name_countries[i])
+  boxplot(stu_behav~cluster, Y, col = "gold", main = name_countries[i])
+  boxplot(teach_behav~cluster, Y, col = "gold", main = name_countries[i])
 }
 
+#EUR
+tot_immig <- length(which(EUR$immigration==2 | EUR$immigration==3))
+immig_1cl <- EUR$immigration[clusters==1]
+immig_2cl <- EUR$immigration[clusters==2]
+immig_3cl <- EUR$immigration[clusters==3]
+immig1 <- length(which(immig_1cl==2 | immig_1cl==3))
+immig2 <- length(which(immig_2cl==2 | immig_2cl==3))
+immig3 <- length(which(immig_3cl==2 | immig_3cl==3))
+
+num_immig1EUR = immig1/tot_immig
+num_immig1EUR
+num_immig2EUR = immig2/tot_immig
+num_immig2EUR
+num_immig3EUR = immig3/tot_immig
+num_immig3EUR
+
+#DNK
+tot_immig <- length(which(DNK$immigration==2 | DNK$immigration==3))
+immig_1cl <- DNK$immigration[clusters==1]
+immig_2cl <- DNK$immigration[clusters==2]
+immig_3cl <- DNK$immigration[clusters==3]
+immig1 <- length(which(immig_1cl==2 | immig_1cl==3))
+immig2 <- length(which(immig_2cl==2 | immig_2cl==3))
+immig3 <- length(which(immig_3cl==2 | immig_3cl==3))
+
+num_immig1DNK = immig1/tot_immig
+num_immig1DNK
+num_immig2DNK = immig2/tot_immig
+num_immig2DNK
+num_immig3DNK = immig3/tot_immig
+num_immig3DNK
+
+#GBR
+tot_immig <- length(which(GBR$immigration==2 | GBR$immigration==3))
+immig_1cl <- GBR$immigration[clusters==1]
+immig_2cl <- GBR$immigration[clusters==2]
+immig_3cl <- GBR$immigration[clusters==3]
+immig1 <- length(which(immig_1cl==2 | immig_1cl==3))
+immig2 <- length(which(immig_2cl==2 | immig_2cl==3))
+immig3 <- length(which(immig_3cl==2 | immig_3cl==3))
+
+num_immig1GBR = immig1/tot_immig
+num_immig1GBR
+num_immig2GBR = immig2/tot_immig
+num_immig2GBR
+num_immig3GBR = immig3/tot_immig
+num_immig3GBR
